d2b13f0910b80df9c809c2069c3bfc349bd361f3
Bryan <jbsg97@gmail.com>
2021-04-22 17:55:44 -0600

    feat: Templates and requirements added

--- src/server.py
+++ src/server.py
@@ -1,7 +1,7 @@
-from flask import Flask
+from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='./app/templates/', static_folder='./app/static/')
 
 @app.route("/")
-def health_check():
-    return "Welcome to my portfolio"
+def index():
+    return render_template('index.html')
